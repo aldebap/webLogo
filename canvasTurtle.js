a2605716e9d9ba0b19e412514da69f09d20eb5f5
Aldebaran Perseke <aldeba_bla@yahoo.com>
2016-08-12 20:08:13 +0000

    Implementação da interface para ediçao do fonte Logo Implementação do canvas para a tartaruga Logo

--- canvasTurtle.js
+++ canvasTurtle.js
@@ -1,10 +1,41 @@
 /* *
+	 * canvas turtle initialization
+	 */
+
+var turtleCanvas;
+var	canvasContext;
+var	turtleX;
+var	turtleY;
+var	turtleAngle;
+
+function canvasTurtleInit() {
+	turtleCanvas = document.getElementById( "canvasTurtle" );
+	canvasContext = turtleCanvas.getContext( "2d" );
+	
+	turtleX = turtleCanvas.width / 2;
+	turtleY = turtleCanvas.height / 2;
+	turtleAngle = 0;
+}
+
+/* *
+	 * clear the canvas
+	 */
+
+function clearCanvas() {
+	canvasContext.fillStyle = "#FFFFFF";
+	canvasContext.fillRect( 0, 0, turtleCanvas.width, turtleCanvas.height );
+}
+
+/* *
+	 * Show turtle
+	 */
+
+function showTurtle() {
+}
+
+/* *
 	 * Turtle Forward
 	 */
 
 function turtleForward() {
-	var canvas = document.getElementById( 'canvas' );
-
-	console.innerHTML = "<br />";
 }
-
--- webLogo.js
+++ webLogo.js
@@ -1,11 +1,21 @@
 /* *
+	 * webLogo init
+	 */
+
+function webLogoInit() {
+}
+
+/* *
 	 * Clean up the editor to start a new Logo script
 	 */
 
 function newScript() {
-	var console = document.getElementById( 'console' );
+	var logoSource = document.getElementById( "edt_logoSource" );
 
-	console.innerHTML = "<br />";
+	logoSource.value = "";
+	
+	canvasContext.fillStyle = "#FFFFFF";
+	canvasContext.fillRect( 0, 0, turtleCanvas.width, turtleCanvas.height );
 }
 
 /* *
@@ -13,18 +23,25 @@ function newScript() {
 	 */
 
 function loadScript() {
-	var console = document.getElementById( 'console' );
+	var logoSource = document.getElementById( "edt_logoSource" );
+	var turtleCanvas = document.getElementById( "canvasTurtle" );
+	var	canvasContext = turtleCanvas.getContext( "2d" );
 
-	console.innerHTML = "FORWARD 50<br />";
+	logoSource.value = "FORWARD 50";
+	
+	canvasContext.fillStyle = "#FFFFFF";
+	canvasContext.fillRect( 0, 0, turtleCanvas.width, turtleCanvas.height );
 }
 
 /* *
-	 * Run the Logo script file from the editor
+	 * Run the Logo script from the editor
 	 */
 
 function runScript() {
-	var console = document.getElementById( 'console' );
-
-	console.innerHTML = "FORWARD 50<br />";
-	console.innerHTML += "running ...<br />";
+	var logoSource = document.getElementById( "edt_logoSource" );
+	var turtleCanvas = document.getElementById( "canvasTurtle" );
+	var	canvasContext = turtleCanvas.getContext( "2d" );
+	
+	canvasContext.font = "30px Arial";
+	canvasContext.fillText( "running ..." + logoSource.value, 10, 50 );
 }
