c91a991c94fe53668de2b3828fb06416aae26bd0
aldebap <aldeba_bla@yahoo.com>
2018-11-27 00:16:18 -0200

    Adjusts to the webServer

--- src/urls.py
+++ src/urls.py
@@ -1,16 +1,22 @@
-#!	/usr/bin/python3
+"""foo URL Configuration
 
-################################################################################
-#	urls.py  -  Nov-25-2018 by aldebap
-#
-#	URLs for the "Web LOGO" web server
-################################################################################
-
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.urls import path
-#from django.conf.urls import path
 
-import views
+import  views
 
-urlpatterns = (
+urlpatterns = [
     path( 'index/', views.index ),
-)
+]
