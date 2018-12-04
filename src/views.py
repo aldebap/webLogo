7237b05284588ecc22741713ffbe31d2098a3785
aldebap <aldeba_bla@yahoo.com>
2018-12-03 23:57:45 -0200

    Moving the Logo parser to server side

--- src/views.py
+++ src/views.py
@@ -13,4 +13,15 @@ def index( _request ):
     return render( _request, 'webLogo.html', {} )
 
 def logoParser( _request ):
+    if "POST" == _request.method:
+        if "square" == _request.POST:
+            return HttpResponse( """clearCanvas();
+showTurtle();
+penDown();
+    
+for( var i = 1; i <= 4; i++ ) {
+    turtleForward( 50 );
+    turtleLeft( 90 );
+}""" )
+
     return HttpResponse( 'LOGO --> JavaScript' )
--- webapp/js/logoInterpreter.js
+++ webapp/js/logoInterpreter.js
@@ -2,6 +2,7 @@
 	 * Interpret a Logo script
 	 */
 
+/*
 function parseScript( _logoScript ) {
     if( _logoScript == "square" ) {
         //  draw a square
@@ -28,4 +29,19 @@ function parseScript( _logoScript ) {
             }, 500 );
         }
     }
+}
+*/
+
+function parseScript(_logoScript) {
+    //  const userAction = async () => {
+    const response = await fetch('http://localhost:8000/logoparser/', {
+        method: 'POST',
+        body: _logoScript,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const myResponse = await response.json(); //extract JSON from the http response
+    // do something with myJson
 }
