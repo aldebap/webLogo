02795ef78c6f97649bc3555bc11aa9f23282f141
aldebap <aldeba_bla@yahoo.com>
2018-12-05 00:40:25 -0200

    Calling the parser service from the GUI

--- webapp/js/logoInterpreter.js
+++ webapp/js/logoInterpreter.js
@@ -33,15 +33,27 @@ function parseScript( _logoScript ) {
 */
 
 function parseScript(_logoScript) {
-    //  const userAction = async () => {
-    const response = await fetch('http://localhost:8000/logoparser/', {
-        method: 'POST',
-        body: _logoScript,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
 
-    const myResponse = await response.json(); //extract JSON from the http response
-    // do something with myJson
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("POST", "http://localhost:8000/logoparser/", true);
+    //    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
+    //    xmlhttp.onload = () => {
+    //        console.log("object program: \"" + this.responseText + "\"");
+    //    }
+    xmlhttp.onreadystatechange = () => {
+        if (4 === this.readyState && 200 === this.status) {
+            console.log("object program: \"" + this.responseText + "\"");
+        }
+    };
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    params = "csrfmiddlewaretoken=" + document.getElementsByName("csrfmiddlewaretoken")[0].value +
+        "&script=" + _logoScript;
+    xmlhttp.send(params);
+}
+
+function callbackFunction(_xmlhttp) {
+    //    if (200 == _xmlhttp.status) {
+    console.log("object program: \"" + _xmlhttp.responseText + "\"");
+    //    }
 }
